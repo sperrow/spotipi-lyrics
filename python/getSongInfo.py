bedf2495ed4a6b3003ab071cf84d6da271867fae
Matthew Sperry <mattjsperry@gmail.com>
2023-05-04 03:05:12 -0700

    return full track info

--- python/getSongInfo.py
+++ python/getSongInfo.py
@@ -1,15 +1,9 @@
-import logging
 import spotipy
 import spotipy.util as util
-
-import requests
-from io import BytesIO
-from PIL import Image
 
 def getSongInfo(username, token_path):
   scope = 'user-read-currently-playing'
   token = util.prompt_for_user_token(username, scope, cache_path=token_path)
-  print("hello")
   if token:
       sp = spotipy.Spotify(auth=token)
       result = sp.current_user_playing_track()
@@ -18,9 +12,8 @@ def getSongInfo(username, token_path):
          print("No song playing")
       else:  
         song = result["item"]["name"]
-        imageURL = result["item"]["album"]["images"][0]["url"]
-        print(song)
-        return [song, imageURL]
+        print('getSongInfo:', song)
+        return result
   else:
       print("Can't get token for", username)
       return None
